951b0fd6ee8a84d65dd8ceb3686a0b6376bbd678
Michael J Maggs <michaelmaggs@gmail.com>
2019-07-25 16:52:01 -0700

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,55 @@
  for you to use if you need it!
  */
 
+function createEmployeeRecord(arr) {
+    let employee = {}
+    employee.firstName = arr[0]
+    employee.familyName = arr[1]
+    employee.title = arr[2]
+    employee.payPerHour = arr[3]
+    employee.timeInEvents = []
+    employee.timeOutEvents = []
+    return employee
+}
+
+function createEmployees(arg) {
+    let newArr = []
+    arg.forEach(employee => {
+    newArr.push(createEmployeeRecord(employee))
+})
+return newArr
+}
+
+function createTimeInEvent(str) {
+    let time = str.split(' ')
+    let day = time[0]
+    let hour = parseInt(time[1])
+    this.timeInEvents.push({type: "TimeIn", hour: hour, date: day})
+    return this
+}
+
+function createTimeOutEvent(str) {
+    let time = str.split(' ')
+    let day = time[0]
+    let hour = parseInt(time[1])
+    this.timeOutEvents.push({type: "TimeOut", hour: hour, date: day})
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    let timeIn = this.timeInEvents.find(obj => obj.date === date)
+    let timeOut = this.timeOutEvents.find(obj => obj.date === date)
+    let hoursWorked = (timeOut.hour - timeIn.hour)/100
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(date) {
+    let hours = hoursWorkedOnDate.call(this, date)
+    let pay = hours * this.payPerHour
+    return pay
+}
+
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +68,26 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function calculatePayroll(arr) {
+    let rawWages = arr.map(employee => allWagesFor.call(employee))
+    let reducer = (acc, curr) => acc + curr
+    let totalWages = rawWages.reduce(reducer)
+    return totalWages
+}
+
+function findEmployeebyFirstName(arr, firstName) {
+    let employee = arr.find(employee => {
+        return employee.firstName == firstName
+    })
+
+  return employee
+}
+
+
+function createEmployeeRecords(arr) {
+let newEmployees = []
+arr.forEach(employee => newEmployees.push(createEmployeeRecord(employee)))
+return newEmployees
 }
