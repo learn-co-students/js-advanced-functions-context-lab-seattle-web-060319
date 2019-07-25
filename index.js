1317308e2bad7846ec1160628a4b47ad9a439807
kargo <karenrgo@gmail.com>
2019-07-25 14:13:39 -0700

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,75 @@
 /* Your Code Here */
+let createEmployeeRecord = (arr) => {
+    const [firstName, familyName, title, payPerHour] = arr
+    const newRecord = Object.assign({}, { firstName }, { familyName }, { title }, { payPerHour }, { timeInEvents: [] }, { timeOutEvents: [] })
+    return newRecord
+}
+
+let createEmployees = (empArr) => {
+    let newRecords = empArr.map(arr => {
+        return createEmployeeRecord(arr)
+    })
+    return newRecords
+}
+
+function createTimeInEvent(str) {
+    let [date, hour] = str.split(' ')
+    let event = {
+        type: "TimeIn",
+        hour: Number(hour),
+        date: date
+    }
+    this.timeInEvents.push(event)
+    return this
+}
+
+function createTimeOutEvent(str) {
+    let [date, hour] = str.split(' ')
+    let event = {
+        type: "TimeOut",
+        hour: Number(hour),
+        date: date
+    }
+    this.timeOutEvents.push(event)
+    return this
+}
+
+function hoursWorkedOnDate(dateStamp) {
+    let timeIn = this.timeInEvents.find((event) => {
+        return event.date == dateStamp
+    })
+    let timeOut = this.timeOutEvents.find((event) => {
+        return event.date == dateStamp
+    })
+
+    let hoursWorked = (timeOut.hour - timeIn.hour) / 100
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(dateStamp) {
+    return (hoursWorkedOnDate.call(this, dateStamp) * this.payPerHour)
+}
+
+function createEmployeeRecords(arr) {
+    let allNew = arr.map(newEmployeeArr => createEmployeeRecord(newEmployeeArr));
+    return allNew
+}
+
+function findEmployeebyFirstName(srcArray, firstName) {
+    let employee = srcArray.find((employee) => {
+        return employee.firstName == firstName
+    })
+    return employee
+}
+
+function calculatePayroll(employeesArray) {
+    let employees_wages = employeesArray.map(record => {
+        return allWagesFor.call(record)
+    })
+    let rfx = (memo, value) => memo + value
+    let payroll = employees_wages.reduce(rfx)
+    return payroll
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
